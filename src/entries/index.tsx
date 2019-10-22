af725e5402cde19028f4d545742be6469eb4560e
范文华 <531362022@qq.com>
2019-10-22 18:05:11 +0800

    Use redux-persist

--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -2,19 +2,32 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducers } from './reducers';
 import { rootMiddleWares } from './middleware';
 import App from '../components/App';
 import { createReduxStore } from '@redux-model/web';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const store = createReduxStore(
-  rootReducers,
-  compose(applyMiddleware(...rootMiddleWares)),
-);
+const store = createReduxStore({
+  reducers: rootReducers,
+  enhancer: compose(applyMiddleware(...rootMiddleWares)),
+  onCombineReducers: (reducer) => {
+    return persistReducer({
+      key: 'root',
+      storage,
+    }, reducer);
+  },
+});
+
+const persistor = persistStore(store);
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
