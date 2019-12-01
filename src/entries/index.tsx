a450a4913389c4763ce80ddfa81a4e2a2372536b
范文华 <531362022@qq.com>
2019-12-01 20:53:46 +0800

    Upgrade packages version

--- src/entries/index.tsx
+++ src/entries/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import HotReact from 'webpack-genius/hot/react';
 import { applyMiddleware, compose } from 'redux';
 import { rootReducers } from './reducers';
 import { rootMiddleWares } from './middleware';
@@ -21,9 +20,7 @@ const store = createReduxStore({
 ReactDom.render(
   <Provider store={store}>
     <PersistGate>
-      <HotReact>
-        <App />
-      </HotReact>
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById('root'),
